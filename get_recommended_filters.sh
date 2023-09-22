0ed310a1b2bd7d6f891d6d6d3cb08e69402a287c
chuctran <39668756+oldman20@users.noreply.github.com>
2023-09-23 03:11:27 +0700

    Update get_recommended_filters.sh
    Update bigdragon host (maybe no need bigdragon domain?)

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -8,6 +8,7 @@ urls=(
   https://adaway.org/hosts.txt
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/multi.txt
   https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Hosts/GoodbyeAds.txt
